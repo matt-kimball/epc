82f0899ca51795ff9c2ec987e68d2d828edb703a
alexMA <almiddeleer@gmail.com>
2018-10-13 18:07:48 -0400

    Add real puppeteer tests

--- puppeteer.test.js
+++ puppeteer.test.js
@@ -1,9 +1,43 @@
-describe('Puppeteer works', () => {
-  beforeAll(async () => {
-    await page.goto('http://localhost:8080');
+const argenport = `2 Finest Hour (Set1 #130)
+4 Sabotage (Set1 #252)
+4 Crownwatch Paladin (Set1 #139)
+4 Ripknife Assassin (Set1003 #13)
+2 Vanquisher's Blade (Set4 #112)
+2 Annihilate (Set1 #269)
+4 Auric Interrogator (Set1002 #13)
+4 Bloodletter (Set2 #235)
+3 Slay (Set2 #236)
+4 Unseen Commando (Set3 #122)
+2 Valkyrie Enforcer (Set1 #151)
+4 Winchest Merchant (Set4 #126)
+2 Inquisitor's Blade (Set1003 #9)
+4 Sheriff Marley (Set4 #131)
+4 Tavrod, Auric Broker (Set1002 #18)
+7 Justice Sigil (Set1 #126)
+5 Shadow Sigil (Set1 #249)
+3 Argenport Banner (Set2 #231)
+1 Cabal Standard (Set4 #193)
+4 Crest of Vengeance (Set3 #264)
+2 Crownwatch Standard (Set4 #97)
+4 Seat of Vengeance (Set0 #55)
+`;
+
+describe('Deck import', () => {
+  beforeEach(async () => {
+    await page.goto('http://localhost:8081');
   });
 
-  it('should display "eternal" text on page', async () => {
-    await expect(page).toMatch('cards');
+  it('import deck button should open modal', async () => {
+    await page.click('#import-button');
+    await page.waitForSelector('#import-modal', { visible: true });
+  });
+
+  it('should be possible to import a deck', async () => {
+    await expect((await page.$$('.card-count-edit'))).toHaveLength(0);
+    await page.click('#import-button');
+    await page.waitForSelector('#import-modal textarea', { visible: true });
+    await expect(page).toFill('#import-modal textarea', argenport);
+    await page.click('#import-modal-import-button');
+    await expect((await page.$$('.card-count-edit'))).toHaveLength(22);
   });
 });
