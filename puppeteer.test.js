4b14ae90f64c9afa9f209c31c32dde4f0eb0c27b
alexMA <almiddeleer@gmail.com>
2018-10-15 20:37:52 -0400

    Make tests run faster and add test for add a card

--- puppeteer.test.js
+++ puppeteer.test.js
@@ -1,37 +1,9 @@
-const argenport = `2 Finest Hour (Set1 #130)
-4 Sabotage (Set1 #252)
-4 Crownwatch Paladin (Set1 #139)
-4 Ripknife Assassin (Set1003 #13)
-2 Vanquisher's Blade (Set4 #112)
-2 Annihilate (Set1 #269)
-4 Auric Interrogator (Set1002 #13)
-4 Bloodletter (Set2 #235)
-3 Slay (Set2 #236)
-4 Unseen Commando (Set3 #122)
-2 Valkyrie Enforcer (Set1 #151)
-4 Winchest Merchant (Set4 #126)
-2 Inquisitor's Blade (Set1003 #9)
-4 Sheriff Marley (Set4 #131)
-4 Tavrod, Auric Broker (Set1002 #18)
-7 Justice Sigil (Set1 #126)
-5 Shadow Sigil (Set1 #249)
-3 Argenport Banner (Set2 #231)
-1 Cabal Standard (Set4 #193)
-4 Crest of Vengeance (Set3 #264)
-2 Crownwatch Standard (Set4 #97)
-4 Seat of Vengeance (Set0 #55)
-`;
-
-const market = `
+const oneCard = "2 Finest Hour (Set1 #130)";
+const oneCardOneMarket = `${oneCard}
 
 --------------MARKET---------------
 
-1 Slay (Set2 #236)
-1 Valkyrie Enforcer (Set1 #151)
-1 Vanquisher's Blade (Set4 #112)
-1 Inquisitor Makto (Set2 #242)
-1 Argenport Banner (Set2 #231)
-`;
+1 Slay (Set2 #236)`;
 
 describe("Deck import", () => {
     beforeEach(async () => {
@@ -47,9 +19,9 @@ describe("Deck import", () => {
         await expect((await page.$$(".card-count-edit"))).toHaveLength(0);
         await page.click("#import-button");
         await page.waitForSelector("#import-modal textarea", { visible: true });
-        await expect(page).toFill("#import-modal textarea", argenport);
+        await expect(page).toFill("#import-modal textarea", oneCard);
         await page.click("#import-modal-import-button");
-        await expect((await page.$$(".card-count-edit"))).toHaveLength(22);
+        await expect((await page.$$(".card-count-edit"))).toHaveLength(1);
     });
 });
 
@@ -64,8 +36,26 @@ describe("Markets", () => {
         await expect((await page.$$(".card-count-edit"))).toHaveLength(0);
         await page.click("#import-button");
         await page.waitForSelector("#import-modal textarea", { visible: true });
-        await expect(page).toFill("#import-modal textarea", `${argenport}${market}`);
+        await expect(page).toFill("#import-modal textarea", oneCardOneMarket);
         await page.click("#import-modal-import-button");
-        await expect((await page.$$(".card-count-edit"))).toHaveLength(27);
+        await expect((await page.$$(".card-count-edit"))).toHaveLength(2);
+    });
+
+    it("add card should not remove market", async () => {
+        // clear import from previous test
+        await page.click("#clear-button");
+
+        await page.click("#import-button");
+        await page.waitForSelector("#import-modal textarea", { visible: true });
+        await expect(page).toFill("#import-modal textarea", oneCardOneMarket);
+        await page.click("#import-modal-import-button");
+        await expect((await page.$$(".card-count-edit"))).toHaveLength(2);
+        await new Promise((resolve) => setTimeout(resolve, 500)); // animation confusing puppeteer?
+        await page.click("#add-card-button");
+        await page.waitForSelector("#add-card-modal .item:first-child", { visible: true });
+        await page.click("#add-card-modal .item:first-child");
+        await new Promise((resolve) => setTimeout(resolve, 100)); // animation confusing puppeteer?
+        await page.click("#add-card-modal-add-button");
+        await expect((await page.$$(".card-count-edit"))).toHaveLength(3);
     });
 });
