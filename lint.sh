4de0b85190d25305d2ec8e3671334ef221064559
alexMA <almiddeleer@gmail.com>
2019-03-09 18:04:40 -0500

    Clean up instructions for developers

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jslint epc-deck.js epc-graph.js epc-table.js epc-test.js epc-ui.js epc-code.js
+eslint epc-deck.js epc-graph.js epc-polyfill.js epc-table.js epc-test.js epc-ui.js epc-code.js
