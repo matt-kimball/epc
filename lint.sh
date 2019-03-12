d18333aba690f35761c4660dbad35e928ff0cd8b
alexMA <almiddeleer@gmail.com>
2019-03-11 22:26:25 -0400

    refactor

--- lint.sh
+++ lint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # If you pass arguments, this command will try to fix the lint
+files="epc-deck.js epc-graph.js epc-table.js epc-test.js epc-ui.js epc-code.js epc-polyfill.js"
 if [ $# -eq 0 ]
 then
-eslint epc-deck.js epc-graph.js epc-table.js epc-test.js epc-ui.js epc-code.js
+  eslint $files
 else
-eslint --fix epc-deck.js epc-graph.js epc-table.js epc-test.js epc-ui.js epc-code.js
+  eslint --fix $files
 fi
